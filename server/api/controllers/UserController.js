e35c54343352b9036d7c1cebaf471b676c4a9749
Vivek Srivastava <srivastavavivek012@gmail.com>
2023-08-20 21:16:56 +0530

    response

--- server/api/controllers/UserController.js
+++ server/api/controllers/UserController.js
@@ -7,17 +7,14 @@ class UserController {
   static registerUser = asyncHandler(async (req, res) => {
     const { username, email, password, confirm_password } = req.body;
     if (!username || !email || !password || !confirm_password) {
-      res.status(404);
-      throw new Error("All fields are mandatory!");
+      res.status(404).send({ message: "All fields are mandatory!" });
     }
     if (password !== confirm_password) {
-      res.status(404);
-      throw new Error("Passwords did not match!");
+      res.status(404).send({ message: "All fields are mandatory!" });
     }
     const userAvailable = await User.findOne({ email });
     if (userAvailable) {
-      res.status(400);
-      throw new Error("User already registered!");
+      res.status(400).send({ message: "User already registered!" });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -33,14 +30,13 @@ class UserController {
         email: newUser.email,
         username: newUser.username,
       });
-    else throw new Error("User data invalid!");
+    else res.send({ message: "User data invalid!" });
   });
 
   static loginUser = asyncHandler(async (req, res) => {
     const { username, password } = req.body;
     if (!username || !password) {
-      res.status(400);
-      throw new Error("All fields are mandatory!");
+      res.status(400).send({ message: "All fields are mandatory!" });
     }
 
     const user = await User.findOne({ username });
@@ -58,8 +54,7 @@ class UserController {
       );
       res.status(200).json({ accessToken });
     } else {
-      res.status(401);
-      throw new Error("Email or password is not valid!");
+      res.status(401).send({ message: "Email or password is not valid!" });
     }
   });
 
@@ -71,31 +66,27 @@ class UserController {
     const { username, followUser } = req.body;
 
     if (!username || !followUser) {
-      res.status(404);
-      throw new Error("Please provide all details!");
+      res.status(404).send({ message: "Please provide all details!" });
     }
 
     const userToFollowData = await User.find({ username: followUser });
     if (!userToFollowData) {
-      res.status(404);
-      throw new Error("User doesn't exists! Cannot Follow!");
+      res.status(404).send({ message: "User doesn't exists! Cannot Follow!" });
     }
 
     const currentUser = await User.find({ username });
     if (!currentUser) {
-      res.status(404);
-      throw new Error("User not found exists!");
+      res.status(404).send({ message: "User not found exists!" });
     }
 
     // Checking if the current user is already following the intended follow user
     if (
       currentUser?.following?.some((user) => {
         console.log(user, "currentUser");
-        user.username === followUser;
+        return user.username === followUser;
       })
     ) {
-      res.status(400);
-      throw new Error("You are already following this user!");
+      res.status(400).send({ message: "You are already following this user!" });
     }
 
     const updatedFollowings = await User.findOneAndUpdate(
@@ -104,8 +95,7 @@ class UserController {
       { new: true }
     );
     if (!updatedFollowings) {
-      res.status(404);
-      throw new Error("Cannot follow user!");
+      res.status(404).send({ message: "Cannot follow user!" });
     }
     await updatedFollowings.save();
     res.json({ message: "User followed successfully." });
