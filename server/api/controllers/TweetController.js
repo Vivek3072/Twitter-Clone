06f505131d35134ab45a2966d94b83a75f16e39d
Vivek Srivastava <srivastavavivek012@gmail.com>
2023-08-20 18:59:34 +0530

    tweet CRUD

--- server/api/controllers/TweetController.js
+++ server/api/controllers/TweetController.js
@@ -1,9 +1,20 @@
 const Tweet = require("../../models/TweetModel");
 
-class PaymentController {
+class TweetController {
   static async getTweets(req, res) {
     try {
       const tweets = await Tweet.find();
+      res.status(200).json({ tweets });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+
+  static async getMyTweets(req, res) {
+    try {
+      const { username } = req.params;
+      const tweets = await Tweet.find({ username: username });
       res.status(200).json({ tweets });
     } catch (err) {
       console.error(err);
@@ -22,7 +33,61 @@ class PaymentController {
       });
 
       await newTweet.save();
-      res.send(500).json({ message: "Tweeted Succesfully!" });
+      res.status(500).json({ message: "Tweeted Succesfully!" });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+
+  static async updateTweet(req, res) {
+    try {
+      const { username, tweet_message } = req.body;
+      if (!username || !tweet_message)
+        return res
+          .status(400)
+          .json({ message: "Please provide both username and tweet_message." });
+
+      const updatedTweet = await Tweet.findOneAndUpdate(
+        { username: username },
+        { $set: { tweet_message: tweet_message } },
+        { new: true }
+      );
+
+      if (!updatedTweet)
+        return res
+          .status(404)
+          .json({ message: "Tweet not found for the provided username." });
+
+      await updatedTweet.save();
+
+      res.json({ message: "Tweet updated successfully.", tweet: updatedTweet });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+
+  static async deleteTweet(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id)
+        return res
+          .status(400)
+          .json({ message: "Please provide both username and tweet_message." });
+
+      const deletedTweet = await Tweet.findOneAndDelete({
+        _id: id,
+      });
+
+      if (!deletedTweet)
+        return res.status(404).json({
+          message:
+            "Tweet not found for the provided username and tweet_message.",
+        });
+
+      res.json({ message: "Tweet deleted successfully." });
     } catch (err) {
       console.error(err);
       res.status(500).json({ message: "Internal server error" });
@@ -30,4 +95,4 @@ class PaymentController {
   }
 }
 
-module.exports = PaymentController;
+module.exports = TweetController;
--- server/api/routes/tweet.routes.js
+++ server/api/routes/tweet.routes.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const { createTweet, getTweets } = require("../controllers/TweetController");
+const {
+  createTweet,
+  getTweets,
+  getMyTweets,
+  updateTweet,
+  deleteTweet,
+} = require("../controllers/TweetController");
 const validateToken = require("../../middlewares/ValidateToken");
 
 router.get("/", getTweets);
+router.get("/:username", getMyTweets);
 router.post("/create", createTweet);
+router.put("/:id", updateTweet);
+router.delete("/:id", deleteTweet);
 
 module.exports = router;
