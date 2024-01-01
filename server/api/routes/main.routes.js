112a59171fb93e81476221b1d5de21a9beb64de0
Vivek Srivastava <srivastavavivek012@gmail.com>
2024-01-01 12:48:13 +0530

    seacr user route updated

--- server/api/routes/main.routes.js
+++ server/api/routes/main.routes.js
@@ -13,13 +13,13 @@ const {
 } = require("../controllers/UserController");
 const validateToken = require("../../middlewares/ValidateToken");
 
-router.get("/all", validateToken, getAllUsers);
 router.post("/register", registerUser);
 router.put("/update-picture", validateToken, updatePicture);
 router.post("/login", loginUser);
 router.post("/follow", validateToken, followUsers);
 router.post("/unfollow", validateToken, unfollowUser);
 router.get("/current", validateToken, currentUser);
-router.get("/search", validateToken, searchUsers);
+router.get("/users", validateToken, searchUsers);
+router.get("/all", validateToken, getAllUsers);
 
 module.exports = router;
