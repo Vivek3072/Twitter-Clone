98fea35d63ac4579655ac7c87575cd1f83648ba9
Vivek Srivastava <srivastavavivek012@gmail.com>
2024-01-05 18:03:59 +0530

    production URL

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
   process.env.NODE_ENV === "production"
-    ? "https://twitter-clone-zwb6.onrender.com/"
+    ? "https://twitter-clone-backend-5jet.onrender.com/"
     : "http://localhost:5001/";
 
 export const socket = io(URL);
